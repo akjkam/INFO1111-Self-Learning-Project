2185589dc564d20c2c4145006ad9bc2342ac1307
akjkam <94522942+akjkam@users.noreply.github.com>
2022-03-09 22:50:01 +1100

    Update layout.js
    Added comments and an ai paddle

--- pong/layout.js
+++ pong/layout.js
@@ -1,15 +1,18 @@
 let userpaddle;
-
+let aipaddle;
+//Define canvas and two paddles
 function setup()
 {
     createCanvas(800, 500);
     userpaddle = new Paddle(25);
+    aipaddle = new Paddle(width - 45);
 }
-
+//Draw onto page
 function draw()
 {
     background(0);
     userpaddle.display();
+    aipaddle.display();
     stroke(255)
     line(width / 2, 0, width / 2, height);
 }
