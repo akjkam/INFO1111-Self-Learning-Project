095c11e77fefd5df5d07bf314eedf8b996f2fde5
akjkam <94522942+akjkam@users.noreply.github.com>
2022-03-09 22:51:57 +1100

    Update paddles.js
    Creating paddle movement

--- pong/paddles.js
+++ pong/paddles.js
@@ -4,14 +4,27 @@ class Paddle
     constructor(x)
     {
         this.x = x;
-        this.y = height / 2
-        this.height = 90
+        this.y = height / 2;
+        this.height = 90;
         this.width = 20;
+        //Define status of paddle(going up or down), initially stationary
+        this.isup = False;
+        this.isdown = False;
     }
     // Create Paddle
     display()
     {
         fill(255);
         rect(this.x, this.y, this.width, this.height);
+    }
+    //Setting paddle movement
+    up()
+    {
+        this.y -= 2;
+    }
+
+    down()
+    {
+        this.y += 2;
     }
 }
