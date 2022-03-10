58087d23f4d9fd98001eb71dcf37da70ef1582d6
akjkam <94522942+akjkam@users.noreply.github.com>
2022-03-10 22:46:17 +1100

    Update layout.js
    added user keyboard input, allowing player to move paddle up and down and displayed ball

--- pong/layout.js
+++ pong/layout.js
@@ -1,18 +1,64 @@
 let userpaddle;
 let aipaddle;
+let ball;
 //Define canvas and two paddles
 function setup()
 {
     createCanvas(800, 500);
+    //Create balls with x position values
     userpaddle = new Paddle(25);
     aipaddle = new Paddle(width - 45);
+    ball = new Ball();
 }
+
 //Draw onto page
 function draw()
 {
     background(0);
     userpaddle.display();
     aipaddle.display();
+    //Draw net/line
     stroke(255);
     line(width / 2, 0, width / 2, height);
+    //Move paddle based on user input within canvas
+    if (userpaddle.isup == true)
+    {
+        if (userpaddle.y > 0)
+        {
+            userpaddle.up();
+        }
+    }
+    else if (userpaddle.isdown == true)
+    {
+        if (userpaddle.y < height - userpaddle.height)
+        {
+            userpaddle.down();
+        }
+    }
+    ball.display();
+
+}
+
+//User input
+function keyPressed()
+{
+    if (keyCode == UP_ARROW)
+    {
+        userpaddle.isup = true;
+    }
+    else if (keyCode == DOWN_ARROW)
+    {
+        userpaddle.isdown = true;
+    }
+}
+function keyReleased()
+{
+    if (keyCode == UP_ARROW)
+    {
+        userpaddle.isup = false;
+    }
+    else if (keyCode == DOWN_ARROW)
+    {
+        userpaddle.isdown = false;
+    }
 }
